942d1d781880893379701a505a5a488100f7be5c
chandler6 <chandler.goddard1@gmail.com>
2014-08-19 12:16:42 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -1,10 +1,6 @@
 print "A new file!"
- 
-  def square (edges. sides):
-  angle = 360.0/edges
-  for i in range (0, edges)
-  move(size)
-  turn(angle)
-  
-  square (50,45)
+def square(side):
+  for i in range(0,4):
+    move(side)
+    turn(90)
   
