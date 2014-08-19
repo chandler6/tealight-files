a9dca1c8b6a8ebc5f9caace732c4b99e5a485b41
chandler6 <chandler.goddard1@gmail.com>
2014-08-19 15:19:29 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -14,7 +14,7 @@ while left_side()=='wall':
    turn(-1)
 if touch()=='wall':
   turn(2)
-while left_side()= right_side() =='wall':
+while left_side()=='wall':
   move()
   if touch()=='wall':
    turn(-1)
