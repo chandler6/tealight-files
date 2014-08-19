7a4ea22a7b1db9faa5f202b41045da0031223053
chandler6 <chandler.goddard1@gmail.com>
2014-08-19 14:57:37 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -7,7 +7,6 @@ from tealight.robot import (move,
                             right_side)
 
 if left_side()wall:
-
-distance=(7)
-for n in range (0, distance):
+  distance=(7)
+  for n in range (0, distance):
   move 7
