7f3093f042deccbfa197f804e029dbd2b98b451d
chandler6 <chandler.goddard1@gmail.com>
2014-08-19 11:58:40 +0100

    Running logo/fractal.py

--- logo/fractal.py
+++ logo/fractal.py
@@ -11,7 +11,7 @@ def segment(scale, detail):
     turn(-60)
     segment(scale / 3.0, detail - 1)
     turn(120)
-    segment(scale / 2.0, detail - 1)
+    segment(scale / 3.0, detail - 1)
     turn(-60)
     segment(scale / 2.0, detail - 1)
     
