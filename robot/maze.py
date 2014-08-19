4a2e72981d0d22dcf940544e500be40e23ca2aa3
chandler6 <chandler.goddard1@gmail.com>
2014-08-19 15:05:03 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -7,9 +7,6 @@ from tealight.robot import (move,
                             right_side)
 
 
-distance=(8)
-for n in range(0,distance):
-  move()
 
-if touch ()== 'wall':
-  turn(2)
+while left_side()=='wall':
+  move()
