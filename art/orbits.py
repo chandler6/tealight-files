1bccb14b68cae47d3c4ea26cb03ace2642467d2d
chandler6 <chandler.goddard1@gmail.com>
2014-08-20 11:55:36 +0100

    Running art/orbits.py

--- art/orbits.py
+++ art/orbits.py
@@ -35,7 +35,7 @@ def handle_frame():
   
   color("red")
   
-  animals/Lion.png(x,y,8)
+  spot(x,y,8)
   vx = vx + ax
   vy = vy + ay
   
@@ -44,6 +44,6 @@ def handle_frame():
   
   color("black")
   
-  animals/Lion.png(x,y,16)
+  spot(x,y,16)
   
   
