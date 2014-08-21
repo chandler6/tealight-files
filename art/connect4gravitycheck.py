e14905761caeea76c1714536ae8432cb583e779c
chandler6 <chandler.goddard1@gmail.com>
2014-08-21 14:39:57 +0100

    Running art/connect4gravitycheck.py

--- art/connect4gravitycheck.py
+++ art/connect4gravitycheck.py
@@ -11,22 +11,20 @@ ay = 0
 def handle_mousedown(x,y,button):
   if button == "left":
    def handle_frame():
+   color("white")
   
-  
-  color("white")
-  
-  spot(x,y,8)
+   spot(x,y,8)
  
-  vx = (vx+ax)*0.97
-  vy = vy + ay +0.12
-  if vy > 10:
+   vx = (vx+ax)*0.97
+   vy = vy + ay +0.12
+   if vy > 10:
     vy =10
   
-  x = x + vx
-  y = y + vy
-  if y >= 800:
-    vy=-1*abs(0.4*vy)
-    if y>805:
+   x = x + vx
+   y = y + vy
+   if y >= 800:
+     vy=-1*abs(0.4*vy)
+     if y>805:
       y=805
   
   color("blue")
