4c89903c4732e6c30eb0b689248fa36f96c661c8
chandler6 <chandler.goddard1@gmail.com>
2014-08-21 10:42:56 +0100

    Running art/orbits.py

--- art/orbits.py
+++ art/orbits.py
@@ -26,9 +26,9 @@ def handle_keyup(key):
   global ax, ay
 
   if key == "left" or key == "right":
-    ax = 50
+    ax = 0
   elif key == "up" or key == "down":
-    ay = 50
+    ay = 0
     
 def handle_frame():
   global x,y,vx,vy,ax,ay
