0995be61b90d02ac8f05a2586dc486320166459a
chandler6 <chandler.goddard1@gmail.com>
2014-08-21 11:59:55 +0100

    Running art/connectfourmain.py

--- art/connectfourmain.py
+++ art/connectfourmain.py
@@ -1,6 +1,6 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 turn = 1
-if def handle_mousedown(x,y,button):
+def handle_mousedown(x,y,button):
   turn = 2
 def handle_mousedown(x,y,button):
   if button == "left":
