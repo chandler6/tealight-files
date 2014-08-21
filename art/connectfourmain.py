a389b3476dbdbe843d6327d5198dde5c89390252
chandler6 <chandler.goddard1@gmail.com>
2014-08-21 14:52:15 +0100

    Running art/connectfourmain.py

--- art/connectfourmain.py
+++ art/connectfourmain.py
@@ -24,12 +24,12 @@ def handle_mousedown(x,y,button):
   global turn
   if button == "left" and turn == 1:
     color("red")
-    spot(x,y,35)
+    spot(x,y,40)
     
     turn = 2
   elif button == "right"and turn == 2:
     color("yellow")
-    spot(x,y,35)
+    spot(x,y,40)
     turn = 1
     
 
