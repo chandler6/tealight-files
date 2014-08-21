9cff7f2c0e08420ea82ddcd66cd11a597332a1a9
chandler6 <chandler.goddard1@gmail.com>
2014-08-21 14:33:41 +0100

    Running art/connect4gravitycheck.py

--- art/connect4gravitycheck.py
+++ art/connect4gravitycheck.py
@@ -1,6 +1,12 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 from tealight.art import (screen_width, screen_height)
 
+x = 600
+y = 400
+vx = 0
+vy = 0
+ax = 0
+ay = 0
 
 
 def handle_frame():
