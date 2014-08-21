5417bdfa84534c03c27626f69b9b9fc86a48c358
chandler6 <chandler.goddard1@gmail.com>
2014-08-21 15:07:21 +0100

    Running art/connect4gravitycheck.py

--- art/connect4gravitycheck.py
+++ art/connect4gravitycheck.py
@@ -15,16 +15,16 @@ def handle_mousedown(x,y,button):
   
    spot(x,y,8)
    x = 10 
-vx = (vx+ax)*0.97
-vy = vy + ay +0.12
-if vy > 10:
-  vy =10
-  x = x + vx
-y = y + vy
-if y >= 800:
-  vy=-1*abs(0.4*vy)
-if y>805:
-  y=805
+   vx = (vx+ax)*0.97
+   vy = vy + ay +0.12
+   if vy > 10:
+     vy =10
+     x = x + vx
+     y = y + vy
+   if y >= 800:
+     vy=-1*abs(0.4*vy)
+   if y>805:
+     y=805
   
  
 
