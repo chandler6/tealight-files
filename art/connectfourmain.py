f72c2c441e06726e5a7ceb43d573ed33bd1ae95e
chandler6 <chandler.goddard1@gmail.com>
2014-08-21 14:27:17 +0100

    Running art/connectfourmain.py

--- art/connectfourmain.py
+++ art/connectfourmain.py
@@ -1,5 +1,12 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
+x = 600
+y = 400
+vx = 0
+vy = 0
+ax = 0
+ay = 0
+
 #1The board
 
 boardArray =[[0,0,0,0,0,0,0,0],
