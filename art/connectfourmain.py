f3a2131502fa5f2b7c4c31e5e2e488b2163d8171
chandler6 <chandler.goddard1@gmail.com>
2014-08-22 09:12:02 +0100

    Running art/connectfourmain.py

--- art/connectfourmain.py
+++ art/connectfourmain.py
@@ -46,8 +46,6 @@ height = 8
 
 for i in range(0,8):
   for j in range(0,8):
-    if (i + j)%4 ==0:
-      color("white")
-      spot(x + i * 100, y + j * 100, 40)
-    else:
-      spot(x + i * 100, y + j * 100, 40)
+
+    color("white")
+    spot(x + i * 100, y + j * 100, 40)
